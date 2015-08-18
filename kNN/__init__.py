210e58e4d83bcc42ffc0be075f33f2b96e5fdf49
Phoenix1327 <phoenix19941327@gmail.com>
2015-08-19 00:16:39 +0800

    handwriting data &  add __init__

---
+++ kNN/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import os
+import operator
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from kNN import kNNbase
+
+DATAPATH = 'C:\\Users\\Administrator\\PycharmProjects\\MLA\\kNN\\digits'
+
